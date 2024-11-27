ead2574c2396c1b9f9f4c36f74c146073f24d798
yanuarkholik <yanuar.kholik@javan.co.id>
2024-11-27 20:04:14 +0700

    update testsuites

---
+++ tests/module/sub-module/test_case_1.js
@@ -0,0 +1,25 @@
+const { By, Key, until } = require('selenium-webdriver');
+require('chromedriver');
+
+describe('project_name', function() {
+    describe('module_name', function() {
+        describe('sub-module_name', function() {
+            before(async function() {
+                // before parameters
+        //
+        //
+            })
+            after(async function() {
+                // after parameters
+            })
+            it('[TC0001] Test step 1', async function() 
+            {
+                // test case here
+            })
+            it('[TC0001] Test step 2', async function() 
+            {
+                // test case here
+            })
+        })
+    })
+})
