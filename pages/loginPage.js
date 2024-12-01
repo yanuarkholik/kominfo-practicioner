fc666e78474c6f6c7c0ef18dee15b16599399e1e
yanuarkholik <yanuar.kholik@javan.co.id>
2024-12-02 00:02:20 +0700

    change loginPage function to class

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,39 +1,21 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const helper = require('../../helpers/InitElement');
+const { Builder, By, Key, until, Browser } = require('selenium-webdriver');
+const elementUtils = require('../utils/elementUtils');
 const config = require('../config/config.json')
 
-require('chromedriver');
 
-describe('XL SSP', function() {
-    describe('Login', function() {
-        describe('Login Internal User', function(){
-            before(async function() {
-                // Before all
-            })
-            after(async function() {
-                // await driver.sleep(3000);
-                await driver.quit();
-            })
-            it('[TC0030] Can login using azure', async function() {
+class LoginPage{
+    constructor(driver) {
+        this.driver = driver
+    }
+    async getUrl(url = config.baseUrl) {
+        await this.driver.get(url)
+    }
+    async login(username, password) {
+        this.getUrl()
 
-                /* As an Admin, I can login using azure
-                * Open the login page
-                * Click Login with Azure button
-                * Input a valid email and password
-                * Click the Sign In button
-                * Displays Home page
-                * */
+        // write actual login test case
+    }
+}
 
-                driver = await new Builder().forBrowser(config.browsers.chrome).build();
+module.exports = LoginPage
 
-                let user = process.env.user;
-                let pswd = process.env.pass;
-            
-                await driver.get(config.baseUrl);
-                await driver.manage().window().maximize();
-                
-                await driver.sleep(5000)
-            })
-        })
-    })
-})
